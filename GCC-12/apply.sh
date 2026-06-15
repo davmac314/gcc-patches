59c33df369fb3ad124cfc1b3757e22e80d104491
Davin McCall <davmac@davmac.org>
2026-06-15 21:09:31 +1000

    Add 10 new patches to GCC-12 patch series.
    This includes a fix for building with Glibc 2.42.

--- GCC-12/apply.sh
+++ GCC-12/apply.sh
@@ -23,3 +23,13 @@ patch -p1 < "$PATCHESDIR"/P4-PR107176.patch
 patch -p1 < "$PATCHESDIR"/P5-PR119085.patch
 patch -p1 < "$PATCHESDIR"/P6-PR121370.patch
 patch -p1 < "$PATCHESDIR"/P7-PR121870.patch
+patch -p1 < "$PATCHESDIR"/P8-PR122976.patch
+patch -p1 < "$PATCHESDIR"/P9-fix-build-with-glibc-2.42.patch
+patch -p1 < "$PATCHESDIR"/P10-PR124358.patch
+patch -p1 < "$PATCHESDIR"/P11-PR123818.patch
+patch -p1 < "$PATCHESDIR"/P12-PR120250.patch
+patch -p1 < "$PATCHESDIR"/P13-PR121773.patch
+patch -p1 < "$PATCHESDIR"/P14-PR122104.patch
+patch -p1 < "$PATCHESDIR"/P15-PR123156.patch
+patch -p1 < "$PATCHESDIR"/P16-PR120182.patch
+patch -p1 < "$PATCHESDIR"/P17-PR123602.patch
